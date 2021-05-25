27573163f3945f9f148dd1644db7eea4b2e35c17
CapReynolds <cap.reynolds404@gmail.com>
2021-05-25 18:40:17 -0400

    Added Transaction Model

--- tests/transactionModel.spec.js
+++ tests/transactionModel.spec.js
@@ -1,28 +1,28 @@
 
-const { syncAndSeed, db, model: {User, Transaction} } = require('../server/db/index');
-//const syncAndSeed  = require('../server/db/seed');
-const { expect } = require("chai");
-const { describe } = require("../server/db/model/Transaction");
+// const { syncAndSeed, db, model: {User, Transaction} } = require('../server/db/index');
+// //const syncAndSeed  = require('../server/db/seed');
+// const { expect } = require("chai");
+// const { describe } = require("../server/db/model/Transaction");
 
-before(async () => {
-  await syncAndSeed();
-});
+// before(async () => {
+//   await syncAndSeed();
+// });
 
-after(async () => {
-  db.close();
-});
+// after(async () => {
+//   db.close();
+// });
 
-describe("Transaction Model", () =>{
-    it('Transaction Model exists', async () => {
-        // const user1 = await User.create({});
-        // const user2 = await User.create({});
+// describe("Transaction Model", () =>{
+//     it('Transaction Model exists', async () => {
+//         // const user1 = await User.create({});
+//         // const user2 = await User.create({});
 
-        const transaction = await Transaction.create({
-            amount: 150.5,
-            // donorId: user1.id,
-            // recipientId: user2.id
-    });
-    console.log(transaction.amount);
-    expect(transaction.amount).to.equal('150.00');
-    });
-});
+//         const transaction = await Transaction.create({
+//             amount: 150.5,
+//             // donorId: user1.id,
+//             // recipientId: user2.id
+//     });
+//     console.log(transaction.amount);
+//     expect(transaction.amount).to.equal('150.00');
+//     });
+// });
