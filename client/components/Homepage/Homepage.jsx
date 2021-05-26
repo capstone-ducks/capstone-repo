a60a7e23bb1c6e0c72d488b9821469a3fdf7f235
Anthony <AnthonySgro1995@gmail.com>
2021-05-26 02:04:42 -0400

    homepage kind of

--- client/components/Homepage/Homepage.jsx
+++ client/components/Homepage/Homepage.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import "../../../public/assets/homepage.css";
+import mainImg from "../../../public/images/charity-img.png";
+import { Link, NavLink } from "react-router-dom";
 
 class Homepage extends Component {
     constructor(props) {
@@ -6,7 +9,49 @@ class Homepage extends Component {
         this.state = {};
     }
     render() {
-        return "Homepage";
+        return (
+            <div className="homepage-container">
+                <div className="homepage-init-cont">
+                    <div className="main-item-cont">
+                        <div className="main-text-cont">
+                            <div className="homepage-text">
+                                <div className="homepage-header">
+                                    <h1>
+                                        Send crypto direct,{" "}
+                                        <span className="highlighted-blue">
+                                            to those in need.
+                                        </span>
+                                    </h1>
+                                </div>
+                                <div className="homepage-subheader">
+                                    <p>
+                                        <span className="highlighted-bold">
+                                            Donate with trust, give without
+                                            hassle,
+                                        </span>{" "}
+                                        and{" "}
+                                        <span className="highlighted-bold">
+                                            make a difference with confidence.
+                                        </span>{" "}
+                                        TheGoodBlock's platform gives you a way
+                                        to reach out directly where it matters
+                                        most.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="homepage-btn">
+                                <NavLink to="/sign-in" id="donate-button">
+                                    Donate Now
+                                </NavLink>
+                            </div>
+                        </div>
+                        <div className="main-img-cont">
+                            <img src={mainImg} alt="" id="main-img" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
