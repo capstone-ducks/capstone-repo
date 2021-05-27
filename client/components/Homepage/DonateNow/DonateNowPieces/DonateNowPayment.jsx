4be47791ab540164ede045ca53c5a5bd49abbba8
Anthony <AnthonySgro1995@gmail.com>
2021-05-27 17:44:23 -0400

    now detects meta mask

--- client/components/Homepage/DonateNow/DonateNowPieces/DonateNowPayment.jsx
+++ client/components/Homepage/DonateNow/DonateNowPieces/DonateNowPayment.jsx
@@ -1,4 +1,16 @@
 import React, { Component } from "react";
+import {
+    Form,
+    Input,
+    TextArea,
+    Button,
+    Select,
+    Checkbox,
+    Image,
+    Message,
+    Icon,
+} from "semantic-ui-react";
+import ethereumLogo from "../../../../../public/images/ethereum-logo.svg";
 
 class DonateNowPaymentForm extends Component {
     constructor(props) {
@@ -8,6 +20,8 @@ class DonateNowPaymentForm extends Component {
         };
 
         this.isMetaMaskInstalled = this.isMetaMaskInstalled.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.installMetaMask = this.installMetaMask.bind(this);
     }
 
     async componentDidMount() {
@@ -25,9 +39,106 @@ class DonateNowPaymentForm extends Component {
         });
     }
 
+    installMetaMask() {
+        console.log("INSTALL META MASK");
+    }
+
+    handleSubmit() {
+        console.log("SUBMIT DONATION!");
+    }
+
     render() {
-        const { metaMaskInstalled } = this.state;
-        return "";
+        const { metaMaskInstalled, selectedCurrency } = this.state;
+        console.log(metaMaskInstalled);
+
+        // If meta mask is not installed, show this
+        if (!metaMaskInstalled)
+            return (
+                <Form
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Form.Button
+                        style={{
+                            backgroundColor: "#d76f63",
+                            color: "white",
+                            fontFamily: "lato",
+                            fontWeight: 400,
+                            fontSize: 14,
+                            width: 350,
+                            height: 60,
+                            position: "relative",
+                            right: -3,
+                        }}
+                        size="large"
+                        onClick={this.installMetaMask}
+                    >
+                        Submit Donation
+                    </Form.Button>
+                    <Message>
+                        <Message.Header>
+                            <Icon name="x" color="red" />
+                            MetaMask Not Detected!
+                        </Message.Header>
+                        <p>
+                            We rely on MetaMask to manage your wallet security.
+                            We will never ask you for your private keys. Please
+                            install MetaMask so we can access your ETH balance.
+                        </p>
+                    </Message>
+                </Form>
+            );
+
+        // Otherwise, return this!
+        return (
+            <Form
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Image src={ethereumLogo} size="mini" />
+                <Form.Group grouped>
+                    <Form.Input
+                        label="Amount"
+                        placeholder="0.000"
+                        size="huge"
+                        style={{ width: 100 }}
+                    />
+                </Form.Group>
+                <Form.Button
+                    style={{
+                        backgroundColor: "#2654ba",
+                        color: "white",
+                        fontFamily: "lato",
+                        fontWeight: 400,
+                        fontSize: 14,
+                        width: 350,
+                        height: 60,
+                        position: "relative",
+                        right: -3,
+                    }}
+                    size="large"
+                    onClick={this.handleSubmit}
+                >
+                    Submit Donation
+                </Form.Button>
+                <Message>
+                    <Message.Header>
+                        <Icon name="checkmark" color="green" />
+                        MetaMask Detected!
+                    </Message.Header>
+                    <p>
+                        We rely on MetaMask to manage your wallet security. We
+                        will never ask you for your private keys.
+                    </p>
+                </Message>
+            </Form>
+        );
     }
 }
 
