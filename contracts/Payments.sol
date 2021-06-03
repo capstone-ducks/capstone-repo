074f7d8fa13583752f777ce8ad081762b340865f
CapReynolds <cap.reynolds404@gmail.com>
2021-06-02 22:41:02 -0400

    test donation contract

--- contracts/Payments.sol
+++ contracts/Payments.sol
@@ -1,6 +1,38 @@
 pragma solidity ^0.5.1;
 
 contract Payments {
+  constructor() public {
+        countDonations(10);
+      }
   // state variable stored on blockchain
-   uint transactionCount = 0;
+   uint public transactionCount = 0;
+
+  struct Donation {
+    uint id;
+    uint donationTotal;
+    address donorAddress;
+    address payable recipientAddress;
+  }
+
+  mapping(uint => Donation) public donations;
+
+  function countDonations(string memory donationTotal) public {
+    uint donationTotal = 10;
+    transactionCount ++;
+    donations[transactionCount] = Donation(transactionCount, donationTotal, _content, false);
+  }
+
+  function sendDonation (uint donationId) public payable aboveMinimum() {
+    require(donations[donationId].recipientAddress != donations[donationId].donorAddress, 'cant send until recipient address has been set');
+    // add donation amount to donations
+      donations[donationId].donationTotal += msg.value;
+  }
+
+  //Modifiers 
+  //validations
+  modifier aboveMinimum () {
+      require( msg.value > 0 ether, "donation must be more than 0 ether");
+      _;
+  }
+
 }
