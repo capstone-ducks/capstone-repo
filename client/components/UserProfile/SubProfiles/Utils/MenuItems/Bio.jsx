bb0bbcb38f2a3732b90427866b110e1bb1caa348
Anthony <AnthonySgro1995@gmail.com>
2021-06-04 15:36:49 -0400

    bio has basic info

--- client/components/UserProfile/SubProfiles/Utils/MenuItems/Bio.jsx
+++ client/components/UserProfile/SubProfiles/Utils/MenuItems/Bio.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Container, Header, Grid } from "semantic-ui-react";
 
 class Bio extends Component {
     constructor(props) {
@@ -7,8 +9,76 @@ class Bio extends Component {
     }
 
     render() {
-        return "Bio";
+        const { email, publicKey, cryptoAddress } = this.props.user;
+
+        return (
+            <React.Fragment>
+                <div id="bio-menu">
+                    <Header as="h4" id="basic-information-header">
+                        DONOR INFORMATION
+                    </Header>
+                    <Grid columns="equal" padded>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <p className="row-title">Wallet Address:</p>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <p>{cryptoAddress}</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                    <Header as="h4" id="contact-information-header">
+                        CONTACT INFORMATION
+                    </Header>
+                    <Grid columns="equal" padded>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <p className="row-title">Phone:</p>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <p>No Number Found</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <p className="row-title">Address:</p>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <p>No Address Found</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <p className="row-title">Email:</p>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <p>{email}</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                    <Header as="h4" id="basic-information-header">
+                        BASIC INFORMATION
+                    </Header>
+                    <Grid columns="equal" padded>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <p className="row-title">Gender:</p>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <p>No Information</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            </React.Fragment>
+        );
     }
 }
 
-export default Bio;
+function mapStateToProps(state) {
+    return {
+        user: state.auth.user,
+    };
+}
+
+export default connect(mapStateToProps)(Bio);
