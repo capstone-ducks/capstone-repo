6010107100ddc7be84e33b9080efadee3d915fdf
Anthony <AnthonySgro1995@gmail.com>
2021-06-10 11:20:08 -0400

    recipient menu items added

---
+++ client/components/UserProfile/SubProfiles/Utils/MenuItems/UnclaimedDonations.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UnclaimedDonations extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return "Unclaimed Donations";
+    }
+}
+
+export default UnclaimedDonations;
