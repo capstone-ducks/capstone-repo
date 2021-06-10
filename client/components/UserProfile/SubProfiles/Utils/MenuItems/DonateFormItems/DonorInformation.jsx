556c35c12e8da92538657d7d4e9bdc6b93f470a3
Anthony <AnthonySgro1995@gmail.com>
2021-06-10 15:03:58 -0400

    bio submit available

--- client/components/UserProfile/SubProfiles/Utils/MenuItems/DonateFormItems/DonorInformation.jsx
+++ client/components/UserProfile/SubProfiles/Utils/MenuItems/DonateFormItems/DonorInformation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { genderOptions, raceOptions } from "./genderRaceOptions";
 import {
     Form,
     Input,
@@ -8,29 +8,6 @@ import {
     Header,
     Accordion,
 } from "semantic-ui-react";
-
-const genderOptions = [
-    { key: "m", text: "Male", value: "male" },
-    { key: "f", text: "Female", value: "female" },
-    { key: "t", text: "Transgender", value: "transgender" },
-    { key: "n", text: "Non-Binary", value: "nonbinary" },
-    { key: "o", text: "Other", value: "other" },
-    { key: "x", text: "Prefer Not to Answer", value: "x" },
-];
-const raceOptions = [
-    { key: "w", text: "White", value: "white" },
-    { key: "b", text: "Black / African American", value: "black" },
-    { key: "h", text: "Hispanic or Latino", value: "hispanic" },
-    { key: "a", text: "Asian", value: "asian" },
-    { key: "i", text: "American Indian or Alaska Native", value: "indigenous" },
-    {
-        key: "p",
-        text: "Native Hawaiian or Other Pacific Islander",
-        value: "pacific",
-    },
-    { key: "m", text: "Multiracial", value: "multiracial" },
-    { key: "x", text: "Prefer Not to Answer", value: "x" },
-];
 
 const DonorInformation = (props) => {
     const {
--- client/components/UserProfile/SubProfiles/Utils/MenuItems/Popups/EditBio.jsx
+++ client/components/UserProfile/SubProfiles/Utils/MenuItems/Popups/EditBio.jsx
@@ -1,46 +1,146 @@
-import React from "react";
-import { Button, Header, Image, Modal } from "semantic-ui-react";
+import React, { Component } from "react";
+import {
+    Button,
+    Header,
+    Image,
+    Modal,
+    Input,
+    Select,
+    Form,
+} from "semantic-ui-react";
+import nanPic from "../../../../../../../public/images/profile-pictures/nan.png";
+import { connect } from "react-redux";
+import { genderOptions, raceOptions } from "../DonateFormItems";
 
-const EditBio = ({ trigger }) => {
-    const [open, setOpen] = React.useState(false);
+class EditBio extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            firstName: props.user.firstName,
+            lastName: props.user.lastName,
+            gender: props.user.gender,
+            email: props.user.email,
+            phone: props.user.phone,
+            race: props.user.race,
+        };
 
-    return (
-        <Modal
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={trigger}
-        >
-            <Modal.Header>Select a Photo</Modal.Header>
-            <Modal.Content image>
-                <Image
-                    size="medium"
-                    src="/images/avatar/large/rachel.png"
-                    wrapped
-                />
-                <Modal.Description>
-                    <Header>Default Profile Image</Header>
-                    <p>
-                        We've found the following gravatar image associated with
-                        your e-mail address.
-                    </p>
-                    <p>Is it okay to use this photo?</p>
-                </Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button color="black" onClick={() => setOpen(false)}>
-                    Nope
-                </Button>
-                <Button
-                    content="Yep, that's me"
-                    labelPosition="right"
-                    icon="checkmark"
-                    onClick={() => setOpen(false)}
-                    positive
-                />
-            </Modal.Actions>
-        </Modal>
-    );
-};
+        this.setOpen = this.setOpen.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-export default EditBio;
+    setOpen(bool) {
+        this.setState({ open: bool });
+    }
+
+    handleEdit(e, { name, value }) {
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit() {
+        console.log("hi");
+        this.setOpen(false);
+    }
+
+    render() {
+        const { firstName, lastName, gender, email, phone, race, open } =
+            this.state;
+        console.log(this.props.user, this.state);
+
+        return (
+            <Modal
+                onClose={() => this.setOpen(false)}
+                onOpen={() => this.setOpen(true)}
+                open={open}
+                trigger={this.props.trigger}
+            >
+                <Modal.Header>Select a Photo</Modal.Header>
+                <Modal.Content image>
+                    <Image size="medium" src={nanPic} wrapped />
+                    <Modal.Description>
+                        <Header>Default Profile Image</Header>
+                        <Form>
+                            <Form.Group widths="equal">
+                                <Form.Field
+                                    value={firstName}
+                                    name="firstName"
+                                    control={Input}
+                                    label="First name"
+                                    placeholder="First name"
+                                    onChange={this.handleEdit}
+                                />
+                                <Form.Field
+                                    value={lastName}
+                                    name="lastName"
+                                    control={Input}
+                                    label="Last name"
+                                    placeholder="Last name"
+                                    onChange={this.handleEdit}
+                                />
+                            </Form.Group>
+                            <Form.Group widths="equal">
+                                <Form.Field
+                                    value={gender}
+                                    name="gender"
+                                    control={Select}
+                                    label="Gender"
+                                    options={genderOptions}
+                                    placeholder="Gender"
+                                    onChange={this.handleEdit}
+                                />
+                                <Form.Field
+                                    value={race}
+                                    name="race"
+                                    control={Select}
+                                    label="Race/Ethnicity"
+                                    options={raceOptions}
+                                    placeholder="Race/Ethnicity"
+                                    onChange={this.handleEdit}
+                                />
+                            </Form.Group>
+                            <Form.Group widths="equal">
+                                <Form.Field
+                                    value={email}
+                                    name="email"
+                                    control={Input}
+                                    label="Email"
+                                    placeholder="Email"
+                                    onChange={this.handleEdit}
+                                />
+                                <Form.Field
+                                    value={phone}
+                                    name="phone"
+                                    control={Input}
+                                    label="Phone"
+                                    placeholder="Phone"
+                                    onChange={this.handleEdit}
+                                />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button color="black" onClick={() => this.setOpen(false)}>
+                        Nope
+                    </Button>
+                    <Button
+                        content="Save Changes"
+                        labelPosition="right"
+                        icon="checkmark"
+                        onClick={() => this.handleSubmit()}
+                        positive
+                    />
+                </Modal.Actions>
+            </Modal>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.auth.user,
+    };
+}
+
+export default connect(mapStateToProps)(EditBio);
