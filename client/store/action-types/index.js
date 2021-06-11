8d2c03fb2da6b7e43f5d5d7995fc240356004fc7
Anthony <AnthonySgro1995@gmail.com>
2021-06-11 13:15:48 -0400

    added action types for donations

--- client/store/action-types/index.js
+++ client/store/action-types/index.js
@@ -4,3 +4,8 @@ export const LOAD_USER = "LOAD_USER";
 export const SIGNIN = "SIGNIN";
 export const LOGOUT = "LOGOUT";
 export const UPDATE_USER = "UPDATE_USER";
+
+// action types for donations
+export const GET_ALL_DONATIONS = "GET_ALL_DONATIONS";
+export const GET_ONE_DONATION = "GET_ONE_DONATION";
+export const CREATE_DONATION = "CREATE_DONATION";
