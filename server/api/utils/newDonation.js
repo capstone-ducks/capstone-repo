c9c86396d1877c23ed19c3115d15626adf3d01c7
emilyasaro <35464891+emilyasaro@users.noreply.github.com>
2021-06-20 23:17:16 -0400

    fixed localhost file path

--- server/api/utils/newDonation.js
+++ server/api/utils/newDonation.js
@@ -119,7 +119,7 @@ const newDonation = (name, email, amount, id) => {]
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
           <tr>
             <td align="center" valign="top" style="padding: 36px 24px;">
-              <a href="https://sendgrid.com" target="_blank" style="display: inline-block;">
+              <a href="http://localhost:4500:" target="_blank" style="display: inline-block;">
                 <img src="../../../public/images/logo-dark-letters.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
               </a>
             </td>
@@ -187,7 +187,7 @@ const newDonation = (name, email, amount, id) => {]
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="http://localhost:4500/#/user/${id}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Claim Donation</a>
+                          <a href="localhost:4500/#/user/${id}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Claim Donation</a>
                         </td>
                       </tr>
                     </table>
