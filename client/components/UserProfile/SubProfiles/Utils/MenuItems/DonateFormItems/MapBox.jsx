543886a21db1af272ef16df0d7d7c293181cf8b1
emilyasaro <35464891+emilyasaro@users.noreply.github.com>
2021-06-23 18:40:40 -0400

    seed json file and adding heatmap layer

--- client/components/UserProfile/SubProfiles/Utils/MenuItems/DonateFormItems/MapBox.jsx
+++ client/components/UserProfile/SubProfiles/Utils/MenuItems/DonateFormItems/MapBox.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "../../../../../../../public/assets/mapbox.css";
-import { Form, Input, Icon, Header, Accordion, Dropdown, Segment, Divider } from "semantic-ui-react";
+import { Icon, Header, Accordion, Segment } from "semantic-ui-react";
 import mapboxgl from 'mapbox-gl';
 mapboxgl.accessToken = "pk.eyJ1IjoiZW1pbHlhc2FybyIsImEiOiJja29uODBhZmMwY2xiMndwM3V3dnMxd3JpIn0.-aPNERh8iwRHxLDODbb2ew";
+import recipients from "../../../../../../../server/db/seed/recipient-locations.json";
 
-// TODO change this all to TARGETPOPULATION and make TARGET POPULATION "CONFIRM DONATION"
 
 export default class MapBox extends Component {
   constructor(props) {
@@ -36,7 +36,138 @@ export default class MapBox extends Component {
     });
 
     map.on('load', function () {
-      map.resize();
+      map.resize(); // shifts view when user moves around on map
+
+      map.addSource('recipients', {
+        'type': 'geojson',
+        'data': recipients,
+        });
+
+
+      map.addLayer(
+        {
+          'id': 'recipients-heat',
+          'type': 'heatmap',
+          'source': 'recipients',
+          'maxzoom': 9,
+          'paint': {
+            // Increase the heatmap weight based on frequency and property magnitude
+            'heatmap-weight': [
+              'interpolate',
+              ['linear'],
+              ['get', 'mag'],
+              0,
+              0,
+              6,
+              1
+            ],
+            // Increase the heatmap color weight weight by zoom level
+            // heatmap-intensity is a multiplier on top of heatmap-weight
+            'heatmap-intensity': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              0,
+              1,
+              9,
+              3
+            ],
+            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+            // Begin color ramp at 0-stop with a 0-transparancy color
+            // to create a blur-like effect.
+            'heatmap-color': [
+              'interpolate',
+              ['linear'],
+              ['heatmap-density'],
+              0,
+              'rgba(33,102,172,0)',
+              0.2,
+              'rgb(103,169,207)',
+              0.4,
+              'rgb(209,229,240)',
+              0.6,
+              'rgb(253,219,199)',
+              0.8,
+              'rgb(239,138,98)',
+              1,
+              'rgb(178,24,43)'
+            ],
+            // Adjust the heatmap radius by zoom level
+            'heatmap-radius': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              0,
+              2,
+              9,
+              20
+            ],
+            // Transition from heatmap to circle layer by zoom level
+            'heatmap-opacity': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              7,
+              1,
+              9,
+              0
+            ]
+          }
+        },
+        'waterway-label'
+        );
+
+        map.addLayer(
+          {
+            'id': 'recipients-point',
+            'type': 'circle',
+            'source': 'recipients',
+            'minzoom': 7,
+            'paint': {
+              // Size circle radius by earthquake magnitude and zoom level
+              'circle-radius': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                7,
+                ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+                16,
+                ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+              ],
+              // Color circle by earthquake magnitude
+              'circle-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'mag'],
+                1,
+                'rgba(33,102,172,0)',
+                2,
+                'rgb(103,169,207)',
+                3,
+                'rgb(209,229,240)',
+                4,
+                'rgb(253,219,199)',
+                5,
+                'rgb(239,138,98)',
+                6,
+                'rgb(178,24,43)'
+              ],
+              'circle-stroke-color': 'white',
+              'circle-stroke-width': 1,
+              // Transition from heatmap to circle layer by zoom level
+              'circle-opacity': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                7,
+                0,
+                8,
+                1
+              ]
+            }
+          },
+        'waterway-label'
+        );
   });
   }
   render() {
