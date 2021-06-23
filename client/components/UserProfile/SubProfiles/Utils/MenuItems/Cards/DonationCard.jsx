e5e2d106eb21e0d18c3b483e9bbc5b34a6001771
Anthony <AnthonySgro1995@gmail.com>
2021-06-22 23:39:02 -0400

    added the components for the additional cards, did not actually code them yet

---
+++ client/components/UserProfile/SubProfiles/Utils/MenuItems/Cards/DonationCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DonationCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return "";
+    }
+}
+
+export default DonationCard;
