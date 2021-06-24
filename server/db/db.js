590bfe5905f0af6dfdbf5189b2acb3a03df7f8c9
Anthony <AnthonySgro1995@gmail.com>
2021-06-24 15:12:41 -0400

    changing server ssl for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,11 @@ const Sequelize = require("sequelize");
 
 const db = new Sequelize(
     process.env.DATABASE_URL || "postgres://localhost/capstone",
-    { logging: false },
+    {
+        logging: false,
+        dialect: "postgres",
+        ssl: process.env.DATABASE_URL ? true : false,
+    },
 );
 
 module.exports = db;
