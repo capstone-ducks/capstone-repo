3d4c6cb875d554fa107f0620f58c09c5884268b1
Anthony <AnthonySgro1995@gmail.com>
2021-06-24 11:46:11 -0400

    commented out morgan bc it is a dev dependency

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 4500;
-const morgan = require("morgan");
+// const morgan = require("morgan");
 require("dotenv").config();
 
 let server = "";
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Middleware Logging
-app.use(morgan("dev"));
+// *DEV USE ONLY* deactivated for heroku
+// app.use(morgan("dev"));
 
 // Api Routes
 //
@@ -57,6 +58,7 @@ const init = async () => {
         console.log(error);
     }
 };
+
 init();
 
 process.on("SIGINT", () => {
