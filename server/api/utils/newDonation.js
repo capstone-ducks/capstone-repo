2af1cc6d68155a9a56ecb59b3a5c57f4b2105392
emilyasaro <35464891+emilyasaro@users.noreply.github.com>
2021-06-25 20:32:55 -0400

    fixing typo

--- server/api/utils/newDonation.js
+++ server/api/utils/newDonation.js
@@ -187,7 +187,7 @@ const newDonation = (name, email, amount, id) => {
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="http://thegoodblock.herokuapp.com/#/user/sign-in" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Claim Donation</a>
+                          <a href="http://thegoodblock.herokuapp.com/#/sign-in" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Claim Donation</a>
                         </td>
                       </tr>
                     </table>
