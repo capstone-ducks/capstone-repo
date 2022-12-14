a47faf055993636aa340650ba70fc440330766bc
Anthony <AnthonySgro1995@gmail.com>
2022-12-14 14:53:51 -0500

    updated npm packages

--- server/db/seed/seedUsers.js
+++ server/db/seed/seedUsers.js
@@ -11,22 +11,22 @@ const seedUsers = async () => {
         // Change this to modify number of users
         const NUM_FAKE_USERS = 10;
 
-        // let fakeUsers = [];
-        // for (let i = 0; i < NUM_FAKE_USERS; i++) {
-        //     const isDonor = i > Math.floor(NUM_FAKE_USERS / 2) ? true : false;
+        let fakeUsers = [];
+        for (let i = 0; i < NUM_FAKE_USERS; i++) {
+            const isDonor = i > Math.floor(NUM_FAKE_USERS / 2) ? true : false;
 
-        //     const user = new User({
-        //         firstName: faker.name.firstName(),
-        //         lastName: faker.name.lastName(),
-        //         email: faker.internet.email(),
-        //         password: faker.internet.password(),
-        //         publicKey: faker.random.alphaNumeric(130),
-        //         cryptoAddress: faker.random.alphaNumeric(40),
-        //         isDonor,
-        //     }).save();
+            const user = new User({
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                publicKey: faker.random.alphaNumeric(130),
+                cryptoAddress: faker.random.alphaNumeric(40),
+                isDonor,
+            }).save();
 
-        //     fakeUsers.push(user);
-        // }
+            fakeUsers.push(user);
+        }
 
         // await Promise.all(fakeUsers);
 
